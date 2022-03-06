a09487a1b032d2de698af372a6cc0f50b4eb121b
Avinash Kumar <avi.kumar.2204@gmail.com>
2022-03-06 23:28:28 +0530

    Two Sum

--- src/com/problems/TwoSum.java
+++ src/com/problems/TwoSum.java
@@ -1,10 +1,29 @@
 package com.problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+	}
+
+
+	public static int[] twoSum(int[] nums, int target) {
+		int[] res = {-1,-1};
+		Map<Integer,Integer> numMap = new HashMap<>();
+		for(int i=0;i<nums.length;i++) {
+			if(numMap.containsKey(target - nums[i] )) {
+				res[0] = numMap.get(target - nums[i]);
+				res[1] = i;
+				break;
+			}
+			numMap.put(nums[i], i);
+		}		
+		return res;
 	}
 
 }
